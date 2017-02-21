3d1a30af12f5699761833097d996abef5c86d676
Philip Blaskowski <blask017@morris.umn.edu>
2017-02-20 19:49:21 -0600

    Finally got all the tests to work

--- client/src/app/todos/todo-list.component.ts
+++ client/src/app/todos/todo-list.component.ts
@@ -1,18 +1,29 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TodoListService} from './todo-list.service'
+import {FilterBy} from "../users/filter.pipe";
+import {Todo} from "./todo";
 
 @Component({
     selector: 'todo-list-component',
-    providers: [TodoListService],
+    providers: [ FilterBy ],
     templateUrl: 'todo-list.component.html'
 })
-export class TodoListComponent{
-    private todos: any;
+export class TodoListComponent implements OnInit{
+    public todos: Todo[];
 
-    constructor(private _todoListService: TodoListService){
+    constructor(private todoListService: TodoListService){
 
-        this.todos = _todoListService.getTodos();
+        //this.todos = todoListService.getTodos();
 
+    }
+
+    ngOnInit(): void {
+        this.todoListService.getTodos().subscribe(
+            todos => this.todos = todos,
+            err => {
+                console.log(err);
+            }
+        );
     }
 
 }
